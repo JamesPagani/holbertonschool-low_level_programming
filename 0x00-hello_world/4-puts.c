4f1341b18797474a9cb298174e9bdaa690a7307a
Jaime Andres Galvez Villamarin <1167@holbertonschool.com>
2019-09-18 18:26:39 +0000

    Prints a string fragment using 'puts'

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Holberton[60] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Holberton);
+	return (0);
+}
