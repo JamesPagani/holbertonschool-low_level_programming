33552675407f32b4603528a7ca337125c17899b1
JamesPagani <jaimeagalvezv@gmail.com>
2019-09-24 18:20:06 +0000

    Sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - Takes two numbers and delivers the result
+ * @a: Primer numero
+ * @b: Segundo numero
+ *
+ * Return: The sum of both numbers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
