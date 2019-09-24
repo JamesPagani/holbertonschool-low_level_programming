b2d36920c7c37b7ca305da6e9ef3390a97393b3d
JamesPagani <jaimeagalvezv@gmail.com>
2019-09-24 16:00:56 +0000

    Prints the times table from 00 to 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * times_table - Shows the times table from 0 to 9.
+ *
+ * Return: Nothing.
+ */
+
+void times_table(void)
+{
+	int num1 = 0;
+	int num2 = 0;
+	int result;
+
+	while (num1 <= 9)
+	{
+		while (num2 <= 9)
+		{
+			result = num1 * num2;
+
+			if (result < 10)
+			{
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+
+			if (num2 != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			num2++;
+		}
+		_putchar(10);
+		num1++;
+		num2 = 0;
+	}
+}
