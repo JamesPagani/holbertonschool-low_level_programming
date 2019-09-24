cf90386848f47c643b4b2f8a173ae1781a611f79
JamesPagani <jaimeagalvezv@gmail.com>
2019-09-24 15:17:14 +0000

    Prints and outs the last digit of a given number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,11 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n = -n;
+	int out = n % 10;
 
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	if (out < 0)
+		out = -out;
+
+	_putchar(out + '0');
+	return (out);
 }
