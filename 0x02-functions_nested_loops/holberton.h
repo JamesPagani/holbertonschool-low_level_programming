fa76a18ee69133ebdff07f2cd1513134937e666d
JamesPagani <jaimeagalvezv@gmail.com>
2019-09-24 13:24:02 +0000

    Revision 2 of custom header

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -4,5 +4,8 @@ int _putchar(char c);
 /* For: 1-alphabet.c */
 void print_alphabet(void);
 
-/* For: 2-print_alphabet_x10 */
+/* For: 2-print_alphabet_x10.c */
 void print_alphabet_x10(void);
+
+/* For: 3-islower.c */
+int _islower(int c);
