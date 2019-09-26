e415c904fe06cfefcdabf8686dbdf3a0aec4331d
JamesPagani <jaimeagalvezv@gmail.com>
2019-09-26 16:51:06 +0000

    Custom Header file

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -12,3 +12,12 @@ int mul(int a, int b);
 
 /* For 3-print_numbers.c */
 void print_numbers(void);
+
+/* For 4-print_most_numbers.c */
+void print_most_numbers(void);
+
+/* For 5-more_numbers.c */
+void more_numbers(void);
+
+/* For 6-print_line.c */
+void print_line(int n);
