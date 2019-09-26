1859fc1fe1a9bf0fe00d0816f2e1e0332312627b
JamesPagani <jaimeagalvezv@gmail.com>
2019-09-26 15:48:50 +0000

    Prints per line the numbers 0 to 14, ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - Prints the numbers from 0 to 14 ten times (once per line)
+ *
+ * Return: Nothing
+ */
+
+void more_numbers(void)
+{
+	int ch;
+
+	for (int i = 0; i <= 10; i++)
+	{
+		for (ch = 0; ch < 15; ch++)
+		{
+			if (ch / 10 != 0)
+				_putchar(ch / 10 + '0');
+
+			_putchar(ch % 10 + '0');
+		}
+		_putchar(10);
+	}
+}
