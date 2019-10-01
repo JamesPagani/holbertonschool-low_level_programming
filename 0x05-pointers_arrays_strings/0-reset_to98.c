0ff6fe44f3660f094b1dd98c8c3fcd9603947ddd
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-01 13:42:15 +0000

    Updates the value of a variable via pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Updates the value of a variable using a pointer
+ * @*n: Pointer to an interger of the value to update
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
