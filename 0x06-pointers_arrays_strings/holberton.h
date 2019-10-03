9eed15b290478bee00615de87f09d9e6623faa16
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-03 13:11:25 +0000

    Prototypes of all 0x06 tasks' functions

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,43 @@
+/* Holberton putchar function */
+int _putchar(char c);
+
+/* 0. strcat */
+char *_strcat(char *dest, char *src);
+
+/* 1. strncat */
+char *_strncat(char *dest, char *src, int n);
+
+/* 2. strncpy */
+char *_strncpy(char *dest, char *src, int n);
+
+/* 3. strcmp */
+int _strcmp(char *s1, char *s2);
+
+/* 4. I am kind of paranoid in reverse. I suspect people of plotting to make me
+ * happy
+ */
+void reverse_array(int *a, int n);
+
+
+/* 5. Always look up */
+char *string_toupper(char *);
+
+/* 6. Expect the best. Prepare for the worst. Capitalize on what comes */
+char *cap_string(char *);
+
+/* 7. Mozart composed his music not for the elite, but for everybody */
+char *leet(char *);
+
+/* 8. rot13 */
+char *rot13(char *);
+
+/* 9. Numbers have life; they're not just symbols on paper */
+void print_number(int n);
+
+/* 11. It is the addition of strangeness to beauty that constitutes the romantic
+ * character in art
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+/* 12. Noise is a buffer, more effective than cubicles or booth walls */
+void print_buffer(char *b, int size);
