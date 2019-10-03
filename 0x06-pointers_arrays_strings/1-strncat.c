5da3b30e11b30d76f90c704f1a1c9d9266686ece
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-03 14:48:39 +0000

    Concatenates two strings up to a specified limit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * _strncat - Concatenates a string up to the n byte
+ * @dest: String to concatenate
+ * @src: String used to concatenate
+ *
+ * Return: Destination string (dest)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int count_dest;
+	int count_src;
+
+	count_dest = 0;
+	count_src = 0;
+
+	while (*(dest + count_dest) != '\0')
+	{
+		count_dest++;
+	}
+
+	while (*(src + count_src) != '\0' && count_src < n)
+	{
+		*(dest + count_dest) = *(src + count_src);
+		count_dest++;
+		count_src++;
+	}
+
+	*(dest + count_dest) = '\0';
+
+	return (dest);
+}
