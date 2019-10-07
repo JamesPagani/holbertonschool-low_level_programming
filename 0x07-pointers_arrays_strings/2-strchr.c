dab90390581ab720405ff364de0082e6c03972d4
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-07 19:35:39 +0000

    Finds a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: Source string
+ * @c: Character to locate
+ *
+ * Return: Pointer to the first occurence to the character (NULL if not found)
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0' && *s != c)
+	{
+		s++;
+	}
+
+	return (s);
+}
