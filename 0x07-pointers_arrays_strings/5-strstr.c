95a8eedc2b4d45de54d145a9ce5407f4e626c8bb
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-08 00:43:22 +0000

    Searches for the first substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * _strstr - Finds a substring
+ * @haystack: Input string
+ * @needle: Substring to find in haystack
+ * Return: Pointer to the beginning of the substring or NULL if not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, cond;
+
+	cond = 0;
+
+	while (*haystack != '\0' && cond != 2)
+	{
+		if (*haystack == needle[0])
+		{
+			cond = 1;
+			for (i = 0; needle[i] != '\0' && cond == 1; i++)
+			{
+				if (haystack[i] != needle[i])
+				{
+					cond = 0;
+				}
+				else if (needle[i + 1] == '\0')
+				{
+					cond = 2;
+				}
+			}
+		}
+		haystack++;
+	}
+
+	if (cond != 2)
+		return ('\0');
+
+	return (haystack - 1);
+}
