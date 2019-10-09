121060c5336a20ff1d0353ab3509bf917bb64826
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-09 19:41:04 +0000

    Gets the factorial of a number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,8 +3,8 @@
 /**
  * factorial - Prints the factorial of a number
  * @n: Number to get it's factorial
- * Return: Factorial number of n. If n is zero, it returns 1. If n is lower than
- * zero, returns -1
+ * Return: Factorial number of n. If n is zero, it returns 1. If n is lower
+ * than zero, returns -1
  */
 
 int factorial(int n)
@@ -13,5 +13,6 @@ int factorial(int n)
 		return (-1);
 	else if (n == 0 || n == 1)
 		return (1);
-	return (factorial(n) * factorial(n - 1));
+
+	return (n * factorial(n - 1));
 }
