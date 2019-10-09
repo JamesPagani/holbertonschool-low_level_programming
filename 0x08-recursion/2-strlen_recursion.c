398e8c7e1e85373e03139b23eacf577782995f98
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-09 19:15:31 +0000

    Counts the length of the string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - Counts the length of a string (NULL char not included)
+ * @s: Input string
+ * Return: The length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int sum;
+
+	sum = 0;
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (1 + _strlen_recursion(s));
+}
