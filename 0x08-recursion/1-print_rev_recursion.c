f73b3189e8db7ecd77aba0c8cee8ed7b2ffca1dc
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-09 19:15:05 +0000

    Prints a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: String to print
+ * Return: Nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
