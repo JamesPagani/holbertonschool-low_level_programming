a9d53b8a36bbaaafd525ce260f1b30c9331f6e99
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-11 23:16:42 +0000

    Prints every argument passed, one per line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Entry point. prints all arguments passed
+ * @argc - Amount of arguments passed
+ * @argv - String array of arguments passed
+ * Return: 0 if successful, anything else if error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
