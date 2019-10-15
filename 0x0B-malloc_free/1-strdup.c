afa6fd5c4ec29d4e1231d97ef96e495193915ee1
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-15 20:45:15 +0000

    Duplicates a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strlen - Counts the lenght of the string
+ * @s: String to analyze
+ *
+ * Return: Lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+
+	return (count);
+}
+
+
+/**
+ * _strdup - Copies a string to a newly allocated memory space
+ * @str: String to copy
+ * Return: Pointer to the copy of str
+ */
+
+char *_strdup(char *str)
+{
+	char *nstr;
+	int i, leng;
+
+	leng = _strlen(str) + 1;
+	if (str == NULL)
+		return (NULL);
+	nstr = (char *) malloc(sizeof(char) * leng);
+	if (nstr == NULL)
+		return (NULL);
+
+	for (i = 0; i < leng + 1; i++)
+	{
+		nstr[i] = str[i];
+	}
+	return (nstr);
+}
