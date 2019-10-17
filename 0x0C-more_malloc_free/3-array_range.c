e9390ac8840ef20fc65c5459ff48ebf7ae4e11e5
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-17 21:36:02 +0000

    Creates an array of integers and fills it with a range of numbers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,6 +1,26 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
+ * array_fill - Fills the integer array with a range of numbers
+ * @arr: Integer array to fill
+ * @min: First nubmer in range (included)
+ * @max: Last number in range (included)
+ * Return: Pointer to the filled array
+ */
+
+int *array_fill(int *arr, int min, int max)
+{
+	int i;
+
+	for (i = 0; i + min <= max; i++)
+	{
+		arr[i] = i + min;
+	}
+
+	return (arr);
+}
+
+/**
  * array_range - Creates an integer array and fills it with specified numbers
  * @min: Lowest integer
  * @max: Max integer
@@ -10,20 +30,17 @@
 int *array_range(int min, int max)
 {
 	int *out;
-	int i, size;
+	int size;
 
 	if (min > max)
 		return (NULL);
 
-	size = max - min;
+	size = 1 + (max - min);
 	out = malloc(size * sizeof(int));
 	if (out == NULL)
 		return (NULL);
 
-	for(i = 0; i < size && i + min <= i + max; i++)
-	{
-		out[i] = min + i;
-	}
+	out = array_fill(out, min, max);
 
 	return (out);
 }
