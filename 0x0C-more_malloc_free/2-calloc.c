45530c4346ac6bc086143c0e9dd23f1b1f45d346
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-17 20:24:08 +0000

    Allocates memory space and sets it to zero

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,16 +1,39 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
+ * _memset - Fills memory with a constant byte
+ * @s: Where to fill
+ * @b: Byte used to fill
+ * @n: How many bytes to fill
+ *
+ * Return: Pointer to the filled memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+}
+
+
+/**
  * _calloc - Allocates memory space and returns a pointer to that direction
- * @nmemb: Number of elements to store
+ * @nmeb: Number of elements to store
  * @size: Data type of the elements
  * Return: Pointer to the allocated memory space
  */
 
 void *_calloc(unsigned int nmeb, unsigned int size)
 {
-	int i;
-	void *out;
+	char *out;
 
 	if (nmeb == 0 || size == 0)
 		return (NULL);
@@ -19,9 +42,6 @@ void *_calloc(unsigned int nmeb, unsigned int size)
 	if (out == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmeb; i++)
-	{
-		out[i] = 0;
-	}
+	_memset(out, 0, size);
 	return (out);
 }
