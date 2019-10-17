9f6d42d38c9aae58f07aa18643780f9009cdd7dc
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-17 19:27:35 +0000

    Concatenates a string with n bytes of another

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -103,7 +103,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	if (n >= len2)
-	       	out = malloc(sizeof(char) * (len1 + len2 + 1));
+		out = malloc(sizeof(char) * (len1 + len2 + 1));
 	else
 		out = malloc(sizeof(char) * (len1 + n + 1));
 	if (out == NULL)
