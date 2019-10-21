e41a14191566f2acca748b0c36e2002122c082c6
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-21 22:02:28 +0000

    0x0E Functions and dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,7 +4,14 @@
 /* Holberton's putchar */
 int _putchar(char c);
 
-/* Dog Structure */
+/**
+ * struct dog - General structure of a doggo
+ * @name: Name of the dog
+ * @age: Age of the dog (in human years?)
+ * @owner: Name of the dog's owner
+ *
+ * Description: D O G G O
+ */
 struct dog
 {
 	char *name;
