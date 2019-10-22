8edb9c4d7a41723c25c200cc580ea8cedca77bcd
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-22 01:43:04 +0000

    0x0E Functions and dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -25,7 +25,11 @@ int _putchar(char c);
 /* 0x0E Functions */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/* dog_t *new_dog(char *name, float age, char *owner); */
-/* void free_dog(dog_t *d); */
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+/* 0x0E Backup functions */
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 
 #endif
