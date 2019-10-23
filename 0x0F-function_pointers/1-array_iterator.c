1752d0bf5ac89223556f634a6e59836687846d82
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-23 17:27:27 +0000

    Prints all the elements of an integer array using a specific function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - Prints the elements of a given int array in a given format
+ * @array: An array of integer numbers
+ * @size: The size of the array
+ * @action: Function to use for the array printing
+ * Return: Nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
