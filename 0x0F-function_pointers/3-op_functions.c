ce02dee26b1e3a67c7a4e8fcb592b31c6446b3f3
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-23 21:09:43 +0000

    Task 3 operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+/**
+ * op_add - Performs an addition operation with two numbers
+ * @a: First number
+ * @b: Second number
+ * Return: The sum of a plus b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Performs a substraction operation with two numbers
+ * @a: First number
+ * @b: Second number
+ * Return: The susbstraction of a minus b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Performs a multiplication operation with two numbers
+ * @a: First number
+ * @b: Second number
+ * Return: The multiplication of b times a
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Performs a division operation with two numbers
+ * @a: First number
+ * @b: Second number
+ * Return: The division of a divided by b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Performs a modulus operation with two numbers
+ * @a: First number
+ * @b: Second number
+ * Return: The reminder of a divided by b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
