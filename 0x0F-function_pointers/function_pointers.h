e22a80c999abb7da7e41467e6ed1d27e3657f438
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-23 21:09:10 +0000

    0x0F functions

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -11,6 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 /* 0x0F functions for task 3 */
-
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
 #endif
