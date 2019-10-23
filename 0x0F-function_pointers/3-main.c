578d99df84363b1142813f55f98666416fe14a17
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-23 21:20:00 +0000

    Performs a basic operation

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	if (oper == op_div && num2 == 0)
+	if ((oper == op_div || oper == op_mod) && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
