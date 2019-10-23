fdd23fc15231faa6ab9a10045153043817d0438f
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-23 17:27:03 +0000

    Prints a name using a specific function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - Prints a name (string) in a specific format
+ * @name: Name to print
+ * @f: Pointer to a function that either pritns the name as is or in upper.
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
