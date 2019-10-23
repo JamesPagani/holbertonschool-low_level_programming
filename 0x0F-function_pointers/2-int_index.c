73c84444f475e536616a74a2020a5ff1608af880
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-23 17:28:23 +0000

    Finds a coincidence between an element of an integer arry and a target number, according to a specific function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - Compares if an array of integers contains a specific number
+ * @array: An array of integers
+ * @size: Size of the array
+ * @cmp: Function to use for integer comparison
+ * Return: Index of the first match of integers. -1 if no match is found
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int out;
+
+	if (size <= 0)
+		return (-1);
+
+	out = -1;
+	for (i = 0; i < size && out == -1; i++)
+	{
+		if (cmp(array[i]) != 0)
+			out = i;
+	}
+	return (out);
+}
