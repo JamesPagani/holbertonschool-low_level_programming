f0d9d3a12d13a59b55b5f7cf4ace99bce6d0f552
JamesPagani <jaimeagalvezv@gmail.com>
2019-10-31 16:48:24 +0000

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_list - Prints all the elements of the list_t list
+ * @h: Head/entry point of the list
+ * Return: Amount of elements on the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t e_count;
+
+	while(h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		e_count++;
+		h = h->next;
+	}
+	return (e_count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,5 @@
+# 0x12. C - Singly linked list
+
+A *singly* linked list is a dynamic data structure containing a sequence of elements that contain whatever data you want to AND the link to the next element.
+
+*Made by: Jaime Andres Galvez Villamarin*
