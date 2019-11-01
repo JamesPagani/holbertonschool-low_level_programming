6d2e4803bdec864c51645b06a03b95faf8a1c098
JamesPagani <jaimeagalvezv@gmail.com>
2019-11-01 01:44:38 +0000

    Added NULL check for head

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,8 +6,11 @@
  */
 void free_list(list_t *head)
 {
-	if (head->next != NULL)
-		free_list(head->next);
-	free(head->str);
-	free(head);
+	if (head != NULL)
+	{
+		if (head->next != NULL)
+			free_list(head->next);
+		free(head->str);
+		free(head);
+	}
 }
