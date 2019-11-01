729efec22f4a68c9b6ac2c1ae1607fde8f3dec35
JamesPagani <jaimeagalvezv@gmail.com>
2019-11-01 02:02:40 +0000

    Advanced 0, calls a function before the main one

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * rabbit - A function so fast, it gets executed before the main one does!
+ * Return: Nothing
+ */
+
+void rabbit(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
