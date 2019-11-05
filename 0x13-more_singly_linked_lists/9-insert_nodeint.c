86676898a72337fa05235f81e49c23a132d241f7
JamesPagani <jaimeagalvezv@gmail.com>
2019-11-05 21:06:16 +0000

    Fixed case for index 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,35 @@
 #include "lists.h"
 /**
+ * add_nodeint - Adds a new node at the beginning of a listint_t list
+ * @head: Head of the listint_t list
+ * @n: An element of the node (an integer)
+ * Return: Address of the new node on success. NULL on failure
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		new->next = NULL;
+	}
+	else
+	{
+		new->next = *head;
+		*head = new;
+	}
+
+	return (*head);
+}
+
+/**
  * insert_nodeint_at_index - Insert a node at the EXACT specified index
  * @head: Head of the list
  * @idx: Index to place the node
@@ -18,6 +48,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 
+	if (idx == 0)
+		return(add_nodeint(head, n));
 	while (tmp_h != NULL)
 	{
 		if (i == idx)
