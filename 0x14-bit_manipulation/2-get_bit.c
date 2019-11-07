6ba9f7dfa1a8548b5267d96bd8e5ec361e5367ce
JamesPagani <jaimeagalvezv@gmail.com>
2019-11-07 22:20:34 +0000

    First push, possibility of not knowing how to return -1

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * get_bit - Gets the bit at the specified bit index
+ * @n: Input number
+ * @index: Bit index to check
+ * Return: The bit (either a 1 or 0) or -1 on error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int check = 1 << index;
+
+	if (n & check)
+		return (1);
+	else if (!(n & check))
+		return (0);
+	return (-1);
+}
