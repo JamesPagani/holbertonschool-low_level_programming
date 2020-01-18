fa4e07775931cb3bf714a52cf103f9756b557605
JamesPagani <jaimeagalvezv@gmail.com>
2020-01-18 23:06:01 +0000

    Added malloc fail cases

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,9 +8,14 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *h_table = malloc(sizeof(hash_table_t));
-
+	if (h_table == NULL)
+		return (NULL);
 	h_table->size = size;
 	h_table->array = malloc(sizeof(hash_node_t *) * size);
-
+	if (h_table->array == NULL)
+	{
+		free(h_table);
+		return (NULL);
+	}
 	return (h_table);
 }
