9892fe4a09f917b69a96aae791721a708e7ae44c
= <=>
2020-02-12 18:27:41 +0000

    Restructured the Header file and added the functions used in task 100

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,39 +1,21 @@
 /* General usage */
 int _putchar(char c);
 
-/* For: 1-alphabet.c */
+/* Main functions */
 void print_alphabet(void);
-
-/* For: 2-print_alphabet_x10.c */
 void print_alphabet_x10(void);
-
-/* For: 3-islower.c */
 int _islower(int c);
-
-/* For: 4-isalpha.c */
 int _isalpha(int c);
-
-/* For: 5-sign.c */
 int print_sign(int n);
-
-/* For: 6-abs.c */
 int _abs(int);
-
-/* For: 7-print_last_digit.c */
 int print_last_digit(int);
-
-/* For: 8-24_hours.c */
 void jack_bauer(void);
-
-/* For: 9-times_table.c */
 void times_table(void);
-
-/* For: 10-add.c */
 int add(int, int);
-
-/* For: 11-print_to_98.c */
 void print_to_98(int n);
-
-/* For: 100-times_table.c */
 void print_times_table(int n);
 
+/* Backup functions */
+void print_one_digit(int num);
+void print_two_digits(int num);
+void print_three_digits(int num);
