73b7d84387598cb07023d4624809c2e364331bf1
= <=>
2020-02-22 02:04:15 +0000

    Corrected the conditional

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,7 +12,7 @@ We can assume the following things are true:
 def island_perimeter(grid):
     """Calculate the perimeter of an island."""
     total_p = 0
-    if grid is not None or grid != [[]]:
+    if grid is not None and grid != [[]]:
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == 1:
