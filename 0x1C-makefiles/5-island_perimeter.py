a802b406787a8f20b16f73d1795e5fe7e96e26a4
= <=>
2020-02-22 02:01:20 +0000

    Calculates the perimeter of an island in a grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Island perimeter.
+Calculate the perimeter of an island (if there's any).
+We can assume the following things are true:
+    > There's only one island
+    > The island has no water inside it
+    > No diagonals
+    > Width and height of the grid will not be higher than 100 units.
+"""
+
+
+def island_perimeter(grid):
+    """Calculate the perimeter of an island."""
+    total_p = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                total_p += sum_perim(grid, i, j)
+
+    return total_p
+
+def sum_perim(grid, i, j):
+    """Sum the perimeter on the current land square."""
+    current_p = 0
+    try:
+        if grid[i - 1][j] == 0:
+            current_p += 1
+    except LookupError:
+        current_p += 1
+    try:
+        if grid[i][j + 1] == 0:
+            current_p += 1
+    except LookupError:
+        current_p += 1
+    try:
+        if grid[i + 1][j] == 0:
+            current_p += 1
+    except LookupError:
+        current_p += 1
+    try:
+        if grid[i][j - 1] == 0:
+            current_p += 1
+    except LookupError:
+        current_p += 1
+
+    return current_p
